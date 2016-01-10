661776c1b38b8431696085baf374bc30054dc57d
Luke Sorcelli <luke.sorcelli@everyware.ch>
2016-01-10 18:12:37 +0100

    Added Check if Graphs directory exists

--- pimatelogger.py
+++ pimatelogger.py
@@ -105,7 +105,10 @@ except IOError:
                          "RRA:AVERAGE:0.5:15:2880",
                          "RRA:AVERAGE:0.5:60:8760")
     i=1
-
+try:
+    os.stat(graphpath)
+except:
+    os.mkdir(graphpath)
 
 while i!=0:
     h, t = dht.read_retry(sensor, pin)
